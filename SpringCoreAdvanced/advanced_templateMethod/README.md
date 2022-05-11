3fecd64dd1d9d849326061622b53e4f198e5526d
Dltmd202 <dltmd202@gmail.com>
2022-05-12 00:55:50 +0900

    Feat: Add log tracer with strategy pattern

--- SpringCoreAdvanced/advanced_templateMethod/README.md
+++ SpringCoreAdvanced/advanced_templateMethod/README.md
@@ -740,3 +740,53 @@ public void strategyV1() throws Exception {
 4. `context` 로직 중간에 `strategy.call()` 을 호출해서 주입 받은 `strategy` 로직을 실행한다.
 5. `context` 는 나머지 로직을 실행한다.
 
+
+## 전략 패턴 - 예제2
+
+
+### ContextV1Test - 추가
+
+```java
+@Test
+public void strategyV2() throws Exception {
+    Strategy strategyLogic1 = new Strategy() {
+        @Override
+        public void call() {
+            log.info("비즈니스 로직1 실행");
+        }
+    };
+    ContextV1 context1 = new ContextV1(strategyLogic1);
+    context1.execute();
+
+    Strategy strategyLogic2 = new Strategy() {
+        @Override
+        public void call() {
+            log.info("비즈니스 로직1 실행");
+        }
+    };
+    ContextV1 context2 = new ContextV1(strategyLogic2);
+    context2.execute();
+}
+```
+
+
+#### 정리
+변하지 않는 부분을 `Context` 에 두고 변하는 부분을 `Strategy` 를 구현해서 만든다. 
+그리고 `Context` 의 내부 필드에 `Strategy` 를 주입해서 사용했다.
+
+
+#### 선 조립, 후 실행
+
+
+여기서 이야기하고 싶은 부분은 `Context` 의 내부 필드에 `Strategy` 를 두고 사용하는 부분이다.
+이 방식은 `Context` 와 `Strategy` 를 실행 전에 원하는 모양으로 조립해두고, 그 다음에 `Context` 를 실행하는 선 조립, 후 실행 방식에서 매우 유용하다.
+`Context` 와 `Strategy` 를 한번 조립하고 나면 이후로는 `Context` 를 실행하기만 하면 된다. 우리가
+스프링으로 애플리케이션을 개발할 때 애플리케이션 로딩 시점에 의존관계 주입을 통해 필요한 
+
+의존관계를 모두 맺어두고 난 다음에 실제 요청을 처리하는 것 과 같은 원리이다.
+이 방식의 단점은 `Context` 와 `Strategy` 를 조립한 이후에는 전략을 변경하기가 번거롭다는 점이다. 
+물론 `Context` 에 `setter` 를 제공해서 `Strategy` 를 넘겨 받아 변경하면 되지만, `Context` 를 싱글톤으로 
+사용할 때는 동시성 이슈 등 고려할 점이 많다. 그래서 전략을 실시간으로 변경해야 하면 차라리 이전에 개발한 테스트 코드 
+처럼 `Context` 를 하나더 생성하고 그곳에 다른 `Strategy` 를 주입하는 것이 더 나은 선택일 수 있다.
+
+
--- SpringCoreAdvanced/advanced_templateMethod/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ SpringCoreAdvanced/advanced_templateMethod/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -47,6 +48,57 @@ public class ContextV1Test {
         ContextV1 contextV2 = new ContextV1(strategyLogic2);
         contextV2.execute();
 
+    }
+
+    @Test
+    public void strategyV2() throws Exception {
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+
+    @Test
+    public void strategyV3() throws Exception {
+        ContextV1 context1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context2.execute();
+    }
+
+    @Test
+    public void strategyV4() throws Exception {
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        context1.execute();
+
+
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        context2.execute();
     }
 
 }
