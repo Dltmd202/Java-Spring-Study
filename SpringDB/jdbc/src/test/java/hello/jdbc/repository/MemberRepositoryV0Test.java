72f61eca785f8310be40ea8e3945d672dffbcfe4
Dltmd202 <dltmd202@gmail.com>
2022-05-20 16:54:23 +0900

    Feat: Add JDBC lecture

--- SpringDB/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ SpringDB/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -4,7 +4,10 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -14,12 +17,24 @@ class MemberRepositoryV0Test {
     @Test
     public void crud() throws Exception {
         //save
-        Member member = new Member("memberV1", 10000);
+        Member member = new Member("memberV3", 10000);
         repository.save(member);
 
+        //find
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
         assertThat(findMember).isEqualTo(member);
+
+
+        //update
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 
 }
