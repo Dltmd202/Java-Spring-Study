2c620df20a5e40c1fc3cbb3403fa8f22a41b47f8
Dltmd202 <dltmd202@gmail.com>
2022-08-07 00:31:13 +0900

    Feat: Add spring cloud study

--- cloud/practice/apigateway-service/src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
+++ cloud/practice/apigateway-service/src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
@@ -9,37 +9,33 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-@Slf4j
 @Component
+@Slf4j
 public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
-
-    public GlobalFilter(){
+    public GlobalFilter() {
         super(Config.class);
     }
 
     @Override
     public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> {
+        return ((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Global PRE filter : request id -> {}", config.getBaseMessage());
-
-            if(config.isPreLogger()){
+            log.info("Global Filter baseMessage: {}, {}", config.getBaseMessage(), request.getRemoteAddress());
+            if (config.isPreLogger()) {
                 log.info("Global Filter Start: request id -> {}", request.getId());
             }
-
-            // Custom Post Filter
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                if(config.isPostLogger()){
-                    log.info("Global Filter End: response code = {}", response.getStatusCode());
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                if (config.isPostLogger()) {
+                    log.info("Global Filter End: response code -> {}", response.getStatusCode());
                 }
             }));
-        };
+        });
     }
 
     @Data
-    public static class Config{
+    public static class Config {
         private String baseMessage;
         private boolean preLogger;
         private boolean postLogger;
